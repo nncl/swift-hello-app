cb618c6566810c7a61e7ccd775cac504737b56ff
Usuário Convidado <Guest@PA06MICRO16.local>
2017-02-06 22:51:43 -0200

    Navigation and logging view flow

--- HelloApp/ViewController.swift
+++ HelloApp/ViewController.swift
@@ -12,15 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad() // view já foi carregada, já tá na memória e pronta p ser apresentada para o usuário
+        print("viewDidLoad")
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("super.viewWillAppear(animated)")
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("super.viewDidAppear(animated)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("super.viewWillDisappear(animated)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("super.viewDidDisappear(animated)")
+    }
 
 }
 
